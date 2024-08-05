d8c3777e7d997f7ff75bf1b475d95d1963f16d90
pieceowater <pieceowater@gmail.com>
2024-08-05 11:47:33 +0500

    validation

--- src/services/users/dto/create-user.input.ts
+++ src/services/users/dto/create-user.input.ts
@@ -1,13 +1,24 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
   @Field()
+  @IsNotEmpty()
   username: string;
 
   @Field()
+  @IsEmail()
   email: string;
 
   @Field()
+  @IsNotEmpty()
+  @IsStrongPassword({
+    minLength: 6,
+    minUppercase: 1,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
   password: string;
 }
--- src/services/users/entities/user.entity.ts
+++ src/services/users/entities/user.entity.ts
@@ -1,17 +1,14 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { IsEmail, IsUUID } from 'class-validator';
 
 @ObjectType()
 export class User {
   @Field(() => String)
-  @IsUUID()
   id: string;
 
   @Field()
   username: string;
 
   @Field()
-  @IsEmail()
   email: string;
 
   @Field({ nullable: true })
